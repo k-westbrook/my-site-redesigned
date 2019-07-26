966cacf42f5a74c71ea1b27e5507ed2238e19d55
k-westbrook <kcheriewestbrook@gmail.com>
2019-07-25 23:26:17 -0700

    new projects

--- src/Projects.js
+++ src/Projects.js
@@ -21,8 +21,26 @@ const projects = [
     imageURL: 'https://res.cloudinary.com/dmp2crnzz/image/upload/v1554320022/personal-site/Screen_Shot_2019-04-03_at_12.31.26_PM.png',
     description: 'A written tutorial that explores how to set up a database with MongoDB and utilize Mongoose ',
     link: 'https://medium.com/@kcheriewestbrook/me-and-mongo-a-exploration-of-mongodb-and-mongoose-7c26a24afa39'
+  },
+  {
+    name: 'West Coast Matching Game',
+    imageURL: "https://res.cloudinary.com/dmp2crnzz/image/upload/v1564121444/personal-site/west-coast.png",
+    description: 'A concentration style game that uses React',
+    link: 'https://west-coast-matching-game.herokuapp.com/'
+  },
+  {
+    name: 'Third-Party API: Using OpenWeatherMap to Create a Simple App',
+    imageURL: "https://res.cloudinary.com/dmp2crnzz/image/upload/v1564122005/personal-site/api_medium.png",
+    description: "Exploration of using a third-party api to call and use data for a user's app",
+    link: 'https://medium.com/@kcheriewestbrook/third-party-api-using-openweathermap-to-create-a-simple-app-f3d4d9b3b7f1'
   }
-
+  ,
+  {
+    name: 'react-photo-book-module',
+    imageURL: "https://res.cloudinary.com/dmp2crnzz/image/upload/v1564122176/personal-site/npm%20package.png",
+    description: 'A concentration style game that uses React',
+    link: 'https://www.npmjs.com/package/react-photo-book-module'
+  }
 
 ]
 
