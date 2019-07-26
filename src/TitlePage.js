352becad59173e8ea9af081ab36282c9766dcc1f
k-westbrook <kcheriewestbrook@gmail.com>
2019-07-25 20:46:40 -0700

    updated title photo

--- src/TitlePage.js
+++ src/TitlePage.js
@@ -6,15 +6,16 @@ class TitlePage extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-
-          <div className='double-box-border'>
+        <div className="content-container">
 
 
-            <h2 className='title-header'>Katie Westbrook</h2>
-          </div>
 
-        </header>
+          <img className="seattle-img" src='https://images.pexels.com/photos/1796730/pexels-photo-1796730.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt="seattle" />
+
+          <h2 className='title-header'>Katie Westbrook</h2>
+
+
+        </div>
 
       </div>
     );
